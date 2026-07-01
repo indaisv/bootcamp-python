167985cdd681df889e6e0d560c7d79c7b0f937f3
Viraj Indais <indaisv-indaisviraj@gmail.com>
2026-07-01 18:07:17 +0530

    Day 1 exercises: Added city greeting and age calculator

--- src/hello_bootcamp.py
+++ src/hello_bootcamp.py
@@ -3,21 +3,34 @@ hello_bootcamp.py
 
 My first professional Python program.
 Author: Viraj
-Date: 1-07-26
+Date: 2026-07-01
 """
 
 
-def greet(name: str) -> str:
+def greet(name: str, city: str) -> str:
     """
     Return a professional greeting message.
 
     Args:
         name: The name of the person to greet.
-
+        city: the city of the person.
     Returns:
         A formatted greeting string.
     """
-    return f"Hello, {name}! Welcome to the bootcamp."
+    return f"Hello, {name} from {city}! Welcome to the bootcamp."
+
+def calculated_age(birth_year: int) -> int:
+    """
+    Calculate approximate age based on birth year.
+
+    Args:
+        birth_year: The year the person was born.
+
+    Returns:
+        The approximate age as an integer.
+    """
+    current_year = 2026
+    return current_year - birth_year
 
 
 def main() -> None:
@@ -27,8 +40,15 @@ def main() -> None:
     print("=" * 50)
 
     user_name = input("Enter your name: ")
-    message = greet(user_name)
+    user_city = input("Enter your city: ")
+    message = greet(user_name, user_city)
     print(message)
+    
+    birth_year_input = input("Enter birth year: ")
+    birth_year = int(birth_year_input)
+    age = calculated_age(birth_year)
+    print(f"You are approximately {age} years old.")
+    
 
     print("\nYour environment is ready. Let's build something amazing.")
     print("=" * 50)
